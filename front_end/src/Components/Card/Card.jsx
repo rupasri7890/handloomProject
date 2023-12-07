adf7e3dc485859290ec44b9218d25d09d2fac541
Jhansi Mannidi <jhansi485@sasi.ac.in>
2023-12-07 23:30:10 +0530

    initial commit

---
+++ front_end/src/Components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Card.css'
+
+const Card=(prop)=>{
+    return
+        <div className="card">{props.children}</div>;
+    
+};
+export default Card;
---
+++ front_end/src/utils/database.js
@@ -0,0 +1,14 @@
+export const database=[
+    {
+        username: "user1",
+        password: "password1",
+    },
+    {
+        username:'user2',
+        password: " password23",
+    },
+    {
+        username:'user3',
+        password: " password",
+    }
+]
