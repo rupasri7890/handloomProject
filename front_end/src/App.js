21169f140e7f4720e3e4423eb65ec1555548315e
Jhansi Mannidi <jhansi485@sasi.ac.in>
2023-12-08 13:17:28 +0530

    all

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,40 @@
+import './App.css';
+import profile from "./image/image.jpg";
+import email from "./image/email logo.png";
+import password from "./image/lock logo.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" classNameo="profile"/>
+            </div>
+
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email"/>
+              <input type="text" placeholder="user name" className="name"/>
+            </div>
+            <div className="second-input">
+              <img src={password} alt="password" className="email"/>
+              <input type="text" placeholder="user name" className="name"/>
+            </div>
+            <div className="loginbutton">
+            <button>Login</button>
+            </div>
+            
+              <p className="links">
+                <a href="#">Forgot password? </a> Or <a href="#"> Sign Up</a> 
+              </p>
+            
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- front_end/src/Components/Card/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Card.css'
-
-const Card=(prop)=>{
-    return
-        <div className="card">{props.children}</div>;
-    
-};
-export default Card;
