7a97f5615c57566fde326b679db27a2c19ba2521
CHENNU GANESH <ganeshc@eficensit.com>
2024-01-11 12:37:49 +0530

    add weaver product and getiing products has done

--- front_end/src/Components/WeaverPage.js
+++ front_end/src/Components/WeaverPage.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FaServer } from "react-icons/fa6"
+import { IoIosSearch } from "react-icons/io"
+import { FaCirclePlus } from "react-icons/fa6"
+import { RiDeleteBin6Line } from "react-icons/ri"
+import { FiEdit2 } from "react-icons/fi"
+import Popup from "reactjs-popup"
+import AddDevice from './AddDevice';
 
 const WeaverPage = () => {
   const [devices, setDevices] = useState([]);
@@ -7,7 +14,7 @@ const WeaverPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:8000//weaver/getProductsByEmail?email="+localStorage.getItem("email"))
+        const response = await axios.get(`http://127.0.0.1:8000/weaver/getProductsByEmail/${localStorage.getItem("email")}`);
         setDevices(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -15,26 +22,82 @@ const WeaverPage = () => {
     };
 
     fetchData();
-  }, []);
+  }, []); // empty dependency array means this effect runs once after the initial render
 
   return (
     <div className="weaver-page">
-      <h1>Weaver Page</h1>
+      <nav className="weaver-nav">
+        <div>
+          <FaServer className="weaver-icon" />
+          <p className="weaver-title">Weaver Page</p>
+        </div>
+        <div className="weaver-btns">
+          <div>
+            <label htmlFor="weaverInput">
+              <IoIosSearch className="search-icon" />
+            </label>
+            <input
+              id="weaverInput"
+              type="search"
+              className="weaver-input"
+              placeholder="Search"
+            />
+          </div>
+          <Popup
+            trigger={
+              <button>
+                <FaCirclePlus className="plus-icon" />
+                Add Product{" "}
+              </button>
+            }
+            modal
+            nested
+          >
+            {() => (
+              <div className="modal">
+                <AddDevice />
+                <div>
+                 {/*  <button onClick={() => close()} className="close-btn">
+                    Close{" "}
+                  </button> */}
+                </div>
+              </div>
+            )}
+          </Popup>
+        </div>
+      </nav>
+      <h3 className="active-heading">Active Devices</h3>
       <div className="devices-container">
-        {devices.length > 0 ? (
-          devices.map((device, index) => (
-            <div key={index}>
-              {/* Display device information */}
-              <p>Device Name: {device.name}</p>
-              {/* Add other device information as needed */}
+      {devices.map((device, index) => (
+
+        <div className="device-card">
+          <img src={device.productImage} alt="device" />
+          <div className="device-info">
+            <div>
+              <h4>{device.productName}</h4>
+              <div>
+                <button>
+                  <RiDeleteBin6Line />
+                </button>
+                <button>
+                  <FiEdit2 />
+                </button>
+              </div>
             </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
+            <p className="status">{device.color}</p>
+            <div>
+              <p className="location">{device.price}</p>
+    
+            </div>
+            <p className="status">{device.description}</p>
+          </div>
+        </div>
+          ))}
+
       </div>
     </div>
-  );
+  )
+
 };
 
 export default WeaverPage;
